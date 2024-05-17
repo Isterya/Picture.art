33093b9333b39edf2632947776be5e67ed137ebe
isterya <vash.vybor.1@gmail.com>
2024-05-17 20:31:24 +0200

    sliders are done and added favicon

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 import modals from './modules/modals';
+import sliders from './modules/sliders';
 
 window.addEventListener('DOMContentLoaded', () => {
    'use strict';
    new WOW().init();
 
    modals();
+   sliders('.feedback-slider-item', 'horizonal', '.main-prev-btn', '.main-next-btn');
+   sliders('.main-slider-item', 'vertical');
 });
---
+++ src/js/modules/sliders.js
@@ -0,0 +1,72 @@
+const sliders = (slides, direction, prev, next) => {
+   let slideIndex = 1,
+      pause = false;
+
+   const items = document.querySelectorAll(slides);
+
+   function showSlides(n) {
+      if (n > items.length) {
+         slideIndex = 1;
+      }
+
+      if (n < 1) {
+         slideIndex = items.length;
+      }
+
+      items.forEach((item) => {
+         item.classList.add('animated');
+         item.style.display = 'none';
+      });
+
+      items[slideIndex - 1].style.display = 'block';
+   }
+
+   showSlides(slideIndex);
+
+   function changeSlides(n) {
+      showSlides((slideIndex += n));
+   }
+
+   try {
+      const prevBtn = document.querySelector(prev),
+         nextBtn = document.querySelector(next);
+
+      prevBtn.addEventListener('click', () => {
+         changeSlides(-1);
+         items[slideIndex - 1].classList.remove('slideInLeft');
+         items[slideIndex - 1].classList.add('slideInRight');
+      });
+
+      nextBtn.addEventListener('click', () => {
+         changeSlides(1);
+         items[slideIndex - 1].classList.remove('slideInRight');
+      });
+   } catch (e) {}
+
+   function activateAnimation() {
+      if (direction === 'vertical') {
+         pause = setInterval(function () {
+            changeSlides(1);
+            items[slideIndex - 1].classList.add('slideInDown');
+         }, 3000);
+      } else {
+         pause = setInterval(function () {
+            changeSlides(1);
+            items[slideIndex - 1].classList.remove('slideInLeft');
+            items[slideIndex - 1].classList.add('slideInRight');
+         }, 3000);
+      }
+   }
+
+   activateAnimation();
+
+   items[0].parentNode.addEventListener('mouseenter', () => {
+      clearInterval(pause);
+   });
+
+   items[0].parentNode.addEventListener('mouseleave', () => {
+      activateAnimation();
+   });
+};
+
+export default sliders;

b568494b099a214de24406f4e60a2809d02c2a1b
isterya <vash.vybor.1@gmail.com>
2024-05-19 13:31:55 +0200

    creating cards from the server is also done as an additional option

--- dist/script.js
+++ dist/script.js
@@ -41,6 +41,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
+/* harmony import */ var _services_requests__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../services/requests */ "./src/js/services/requests.js");
+
 const forms = () => {
   const form = document.querySelectorAll('form'),
     inputs = document.querySelectorAll('input'),
@@ -57,13 +59,6 @@ const forms = () => {
   const path = {
     designer: 'assets/server.php',
     question: 'assets/question.php'
-  };
-  const postData = async (url, data) => {
-    let res = await fetch(url, {
-      method: 'POST',
-      body: data
-    });
-    return await res.text();
   };
   const clearInputs = () => {
     inputs.forEach(item => {
@@ -104,7 +99,7 @@ const forms = () => {
       let api;
       item.closest('.popup-design') || item.classList.contains('calc_form') ? api = path.designer : api = path.question;
       console.log(api);
-      postData(api, formData).then(res => {
+      (0,_services_requests__WEBPACK_IMPORTED_MODULE_0__.postData)(api, formData).then(res => {
         console.log(res);
         statusImg.setAttribute('src', message.ok);
         textMessage.textContent = message.success;
@@ -297,6 +292,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
+/* harmony import */ var _services_requests__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../services/requests */ "./src/js/services/requests.js");
+
+
+// В том случае, если используется получение карточек с сервера, мы вместо аргумента "styles" получаем "wrapper".
+// В данном проекте wrapper-ом является селектор "#styles .row".
+
 const showMoreStyles = (trigger, styles) => {
   const cards = document.querySelectorAll(styles),
     btn = document.querySelector(trigger);
@@ -310,6 +311,44 @@ const showMoreStyles = (trigger, styles) => {
     });
     btn.remove();
   });
+
+  //    btn.addEventListener('click', function () {
+  //       getResource('http://localhost:3000/styles')
+  //          .then((res) => createCards(res))
+  //          .catch((error) => {
+  //             showError(error);
+  //             console.log(error);
+  //          });
+
+  //       this.remove();
+  //    });
+
+  //    function createCards(response) {
+  //       response.forEach(({ src, title, link }) => {
+  //          let card = document.createElement('div');
+
+  //          card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+
+  //          card.innerHTML = `
+  //             <div class="styles-block">
+  //                 <img src=${src} alt="style">
+  //                 <h4>${title}</h4>
+  //                 <a href="${link}">Подробнее</a>
+  //             </div>
+  //          `;
+
+  //          document.querySelector(wrapper).appendChild(card);
+  //       });
+  //    }
+
+  //    function showError(error) {
+  //       let errorMessage = document.createElement('div');
+  //       errorMessage.classList.add('error-message', 'animated', 'fadeInUp');
+  //       errorMessage.style.cssText = 'color: red; text-align: center; margin-top: 20px;';
+  //       errorMessage.textContent = 'Что-то пошло не так... Попробуйте позже';
+
+  //       document.querySelector(wrapper).appendChild(errorMessage);
+  //    }
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (showMoreStyles);
 
@@ -383,6 +422,35 @@ const sliders = (slides, direction, prev, next) => {
   });
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (sliders);
+
+/***/ }),
+
+/***/ "./src/js/services/requests.js":
+/*!*************************************!*\
+  !*** ./src/js/services/requests.js ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   getResource: () => (/* binding */ getResource),
+/* harmony export */   postData: () => (/* binding */ postData)
+/* harmony export */ });
+const postData = async (url, data) => {
+  let res = await fetch(url, {
+    method: 'POST',
+    body: data
+  });
+  return await res.text();
+};
+const getResource = async url => {
+  let res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, status ${res.status}`);
+  }
+  return await res.json();
+};
+
 
 /***/ })
 
--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,3 +1,8 @@
+import { getResource } from '../services/requests';
+
+// В том случае, если используется получение карточек с сервера, мы вместо аргумента "styles" получаем "wrapper".
+// В данном проекте wrapper-ом является селектор "#styles .row".
+
 const showMoreStyles = (trigger, styles) => {
    const cards = document.querySelectorAll(styles),
       btn = document.querySelector(trigger);
@@ -13,6 +18,44 @@ const showMoreStyles = (trigger, styles) => {
       });
       btn.remove();
    });
+
+   //    btn.addEventListener('click', function () {
+   //       getResource('http://localhost:3000/styles')
+   //          .then((res) => createCards(res))
+   //          .catch((error) => {
+   //             showError(error);
+   //             console.log(error);
+   //          });
+
+   //       this.remove();
+   //    });
+
+   //    function createCards(response) {
+   //       response.forEach(({ src, title, link }) => {
+   //          let card = document.createElement('div');
+
+   //          card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+
+   //          card.innerHTML = `
+   //             <div class="styles-block">
+   //                 <img src=${src} alt="style">
+   //                 <h4>${title}</h4>
+   //                 <a href="${link}">Подробнее</a>
+   //             </div>
+   //          `;
+
+   //          document.querySelector(wrapper).appendChild(card);
+   //       });
+   //    }
+
+   //    function showError(error) {
+   //       let errorMessage = document.createElement('div');
+   //       errorMessage.classList.add('error-message', 'animated', 'fadeInUp');
+   //       errorMessage.style.cssText = 'color: red; text-align: center; margin-top: 20px;';
+   //       errorMessage.textContent = 'Что-то пошло не так... Попробуйте позже';
+
+   //       document.querySelector(wrapper).appendChild(errorMessage);
+   //    }
 };
 
 export default showMoreStyles;
